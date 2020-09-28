8aeca108813175e11702420007f7741bd52c3698
Surendra Rathaur <surendrarathor87@gmail.com>
2020-09-28 21:31:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+import data from './data/db.json';
+import DataTable from 'react-data-components/lib/DataTable'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super();
+  }
+
+  render() {
+    let columns = [
+      { title: 'Id', prop: 'id' },
+      { title: 'UserId', prop: 'userId' },
+      { title: 'Product Name', prop: 'name' },
+      { title: 'Mode', prop: 'mode' },
+      { title: 'Type', prop: 'type' },
+      { title: 'Destination', prop: 'destination' },
+      { title: 'Origin', prop: 'origin' },
+      { title: 'Total', prop: 'total' },
+      { title: 'Status', prop: 'status' },
+    ];
+
+    return (
+        <DataTable
+          className="container"
+          keys="id"
+          columns={columns}
+          initialData={data.shipments}
+          initialPageLength={10}
+          initialSortBy={{ order: 'descending' }}
+        />
+    );
+  }
 }
 
 export default App;
